f9ef46b717425e3b4290f03cb324b808aaf2c82d
Nia McAlkich <niamcalkich@gmail.com>
2023-05-10 21:48:40 -0400

    Saving again got most functionality done have a few more things to do

--- south-hills-app/src/app/tab4/tab4.page.ts
+++ south-hills-app/src/app/tab4/tab4.page.ts
@@ -8,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class Tab4Page implements OnInit {
 
   constructor() { }
+  campusTour = ''
+  stateCollege = ["Monday 8:00am—5:00pm", "Tuesday 8:00am—5:00pm", "Wednesday 8:00am—7:00pm", "Thursday 8:00am—7:00pm", "Friday 8:00am—5:00pm"]
+  altoona = ["Monday 7:30am—7:00pm", "Tuesday 7:30am—5:00pm", "Wednesday 7:30am—5:00pm", "Thursday 7:30am—5:00pm", "Friday 7:30am—5:00pm"]
+
+  stateCollegeClick() {
+    this.campusTour = this.stateCollege.join(',');
+  }
+
+  altoonaClick() {
+    this.campusTour = this.altoona.join(',');
+  }
 
   ngOnInit() {
   }
